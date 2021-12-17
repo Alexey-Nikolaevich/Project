bf40c9d91f3a986916a674dcdd3a431638b1ba0b
Alexey-Nikolaevich <anbardushko@miem.hse.ru>
2021-12-17 17:48:59 +0300

    Дополнил описание

--- project/Main.py
+++ project/Main.py
@@ -5,28 +5,60 @@ from Settings import*
 from Bird import Bird 
 
 def rule1(bird, flock):
-    """Separate this bird from other birds if they are too close"""
+    """
+    Separate this bird from other birds if they are too close
+    
+    param: bird - current exemplar of class Bird
+    param: flock - other elements of class Bird
+
+    return: None
+    
+    """
     Power_of_ATTRACTION = ATTRACTION * (RADIUS - (bird.get_pos().distance_to(flock.get_pos()))) / RADIUS
     Force = (bird.get_vel() - flock.get_vel()).normalize()
     Force.scale_to_length(Power_of_ATTRACTION)
     bird.push(Force)
 
 def rule2(bird, flock):
-    """Direct this bird in the same direction with other birds"""
+    """
+    Direct this bird in the same direction with other birds
+    
+    param: bird - current exemplar of class Bird
+    param: flock - other elements of class Bird
+
+    return: None
+    
+    """
     Power_of_ATTRACTION = 10 * ATTRACTION * (RADIUS - (bird.get_pos().distance_to(flock.get_pos()))) / RADIUS
     Force = flock.get_vel().normalize()
     Force.scale_to_length(Power_of_ATTRACTION)
     bird.push(Force)
 
 def rule3(bird, flock):
-    """Gather birds together if they are too far from each other"""
+    """
+    Gather birds together if they are too far from each other
+        
+    param: bird - current exemplar of class Bird
+    param: flock - other elements of class Bird
+
+    return: None
+    
+    """
     Power_of_ATTRACTION = ATTRACTION * bird.get_pos().distance_to(flock.get_pos()) / RADIUS
     Force = (flock.get_vel() - bird.get_vel()).normalize()
     Force.scale_to_length(Power_of_ATTRACTION)
     bird.push(Force)
 
 def detect(bird, flock):
-    """Find other birds within th efield of view of this bird"""
+    """
+    Find other birds within th efield of view of this bird
+           
+    param: bird - current exemplar of class Bird
+    param: flock - other elements of class Bird
+
+    return: None
+    
+    """
     distance = bird.get_pos().distance_to(flock.get_pos())
     if distance < VIEW_DISTANCE: 
          return True
@@ -34,7 +66,14 @@ def detect(bird, flock):
         return False
 
 def behavior(bird):
-    """Apply rules for each bird"""
+    """
+    Apply rules for each bird
+    
+    param: bird - current exemplar of class Bird
+
+    return: None
+
+    """
     for flock in birds:
         if flock is not bird:
             if detect(bird, flock):
@@ -43,7 +82,14 @@ def behavior(bird):
                 rule3(bird, flock)
 
 def draw_bird(bird):
-    """Draw birds"""
+    """
+    Draw birds
+
+    param: bird - current exemplar of class Bird
+
+    return: None
+
+    """
     top = bird.get_pos()/1.2 + Vector2(WIDTH//8, HEIGHT//8) - Vector2(pygame.mouse.get_pos())/5
     vert1 = top - bird.get_vel().rotate(-15) * 3
     vert2 = top - bird.get_vel().rotate(15) * 3
@@ -53,6 +99,14 @@ screen = pygame.display.set_mode([WIDTH, HEIGHT]) #New screen
 birds = [Bird() for _ in range(BIRD_NUM)]         #New array of bird
 
 def run():
+    """
+    Run whole application
+
+    param: None
+
+    return: None
+    
+    """
     while True:
         screen.fill((50, 50, 50))
         for event in pygame.event.get():
--- project/Settings.py
+++ project/Settings.py
@@ -1,7 +1,8 @@
-WIDTH = 500             #Witdh of the screen
-HEIGHT = 500            #Height of the screen
-MAX_VEL = 5             #Maximum speed of boid  
-VIEW_DISTANCE = 40      #Distance of view
-ATTRACTION = 0.1        #Rate of power of interaction between boid
-RADIUS = 100            #Distance of interaction between boids
-BIRD_NUM = 50           #Number of boids
+"""Settings of the application"""
+WIDTH = 500             # Witdh of the screen
+HEIGHT = 500            # Height of the screen
+MAX_VEL = 5             # Maximum speed of boid  
+VIEW_DISTANCE = 40      # Distance of view
+ATTRACTION = 0.1        # Rate of power of interaction between boid
+RADIUS = 100            # Distance of interaction between boids
+BIRD_NUM = 50           # Number of boids
