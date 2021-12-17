499a362e82765499ac9f37433baaebf71831fccc
Alexey-Nikolaevich <anbardushko@miem.hse.ru>
2021-12-17 17:51:08 +0300

    Дополнил описание

--- project/Bird.py
+++ project/Bird.py
@@ -8,21 +8,41 @@ from Settings import*
 class Bird:
     """ Describe bird - one flying element"""
     def __init__(self):
-        """Initialize with position and velocity"""
+        """Initialize bird with position(self.pos: pygame.Vectro2) and velocity(self.pos: pygame.Vectro2)"""
         self.pos = Vector2(random.uniform(0,WIDTH), random.uniform(0,HEIGHT))
         self.vel = Vector2(random.uniform(-MAX_VEL, MAX_VEL), random.uniform(-MAX_VEL, MAX_VEL))
-        self.color = (0,0,0)
 
     def get_pos(self):
-        """Return position of the bird"""
+        """
+        Return position of the bird
+        
+        param: self
+
+        return: self.pos
+
+        """
         return self.pos
 
     def get_vel(self):
-        """Return velocity of the bird"""
+        """
+        Return velocity of the bird
+        
+        param: self
+
+        return: self.vel
+
+        """
         return self.vel
     
     def get_color(self):
-        """Return color of the bird"""
+        """
+        Return color of the bird
+
+        param: self
+
+        return: color of bird
+
+        """
         red = int(self.vel.x * self.vel.x) * 10
         if red > 255: red = 254
         green = int(self.vel.y * self.vel.y) * 10
@@ -30,18 +50,30 @@ class Bird:
         return red, green, 50
 
     def push(self, Force):
-        """Change direction of the vector of the velocity"""
+        """
+        Change direction of the vector of the velocity
+        
+        param: self
+
+        return: None
+
+        """
         self.vel += Force
         self.vel += Vector2(random.uniform(-0.05*MAX_VEL, 0.05*MAX_VEL), random.uniform(-0.05*MAX_VEL, 0.05*MAX_VEL))
         if math.sqrt(self.vel.x * self.vel.x + self.vel.y * self.vel.y) >= MAX_VEL:
             self.vel.scale_to_length(MAX_VEL)
 
     def move(self):
-        """Change position of the bird"""
+        """
+        Change position of the bird
+        
+        param: self
+
+        return: None
+
+        """
         self.pos += self.vel
 
-
-        """Conects the ends of the map"""
         if self.pos.x < 0:
             self.pos = Vector2(WIDTH, self.pos.y)
         if self.pos.x > WIDTH:
